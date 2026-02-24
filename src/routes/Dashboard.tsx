e9ea4c48dbf690d2d274311a6496b225e4b887e8
o-curran <oscurran@gmail.com>
2026-02-24 09:41:54 +0000

    visual changes to headings

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -126,7 +126,7 @@ const Dashboard = () => {
             <PerformanceCard debateData={debateArr} />
           </div>
 
-          <div>
+          <div className="mt-6">
             <h2 className="text-3xl sm:text-4xl font-semibold">Positions</h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-6">
               <PieChartPositionCard
